8797112a137714b9aaeaad1880d0988510ef39d3
heymitchfischer <mitchfischer6@gmail.com>
2018-01-22 20:11:51 -0600

    111 - scaled the graph line analyzer to create multiple lines

--- app/controllers/api/v1/codes_controller.rb
+++ app/controllers/api/v1/codes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CodesController < ApplicationController
   def analyze
-    code_analyzer = CodeAnalyzer.new(params[:code])
+    code_analyzer = CodeAnalyzer.new(params[:codes])
     
     render json: {results: code_analyzer.results}
   end
--- app/models/code_analyzer.rb
+++ app/models/code_analyzer.rb
@@ -1,9 +1,9 @@
 class CodeAnalyzer
 
-  attr_reader :code, :graph_data
+  attr_reader :codes, :graph_data
   
-  def initialize(code='')
-    @code = code
+  def initialize(codes=[''])
+    @codes = codes.split(',')
     add_counters_to_code!
     @graph_data = []
   end
@@ -16,12 +16,16 @@ class CodeAnalyzer
   # and y indicates the number of steps it takes for the code to actually run.
 
   def results
-    if @code.index("[*]")
-      [100, 500, 1000, 1500, 2000, 2500, 3000].each do |data|
-        @graph_data << {x: data, y: run_code(@code.gsub("[*]", "#{(1..data).to_a}"))}
+    @codes.each do |code|
+      graph_data = []
+      if code.index("[*]")
+        [100, 500, 1000, 1500, 2000, 2500, 3000].each do |data|
+          graph_data << {x: data, y: run_code(code.gsub("[*]", "#{(1..data).to_a}"))}
+        end
       end
+      @graph_data << graph_data
     end
-
+    p @graph_data
     return @graph_data
   end
 
@@ -43,13 +47,16 @@ class CodeAnalyzer
   # increment count after each line of code runs.
 
   def add_counters_to_code!
-    new_code = "count = 0\n"
-    @code.each_line do |line|
-      new_code += "#{line}\n"
-      new_code += "count += 1\n"
+    temp_codes = []
+    @codes.each do |code|
+      new_code = "count = 0\n"
+      code.each_line do |line|
+        new_code += "#{line}\n"
+        new_code += "count += 1\n"
+      end
+      new_code += "count"
+      temp_codes << new_code
     end
-    new_code += "count"
-    @code = new_code
+    @codes = temp_codes
   end
-
 end
